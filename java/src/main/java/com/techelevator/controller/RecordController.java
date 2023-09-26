5e72f2696c6a771c4c1513b2474f5c57c0e51205
John Joyce <jjoycesommerfeld@gmail.com>
2023-09-26 11:23:12 -0700

    updating

--- java/src/main/java/com/techelevator/controller/RecordController.java
+++ java/src/main/java/com/techelevator/controller/RecordController.java
@@ -1,8 +1,9 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.RecordDao;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.techelevator.model.Record;
+import com.techelevator.model.RecordDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin
@@ -13,6 +14,27 @@ public class RecordController {
 
     public RecordController(RecordDao recordDao) {
         this.recordDao = recordDao;
+    }
+
+    @GetMapping(path = "/record/{id}")
+    public Record getRecordById(@PathVariable String id) {
+        Record returnRecord = new Record();
+        returnRecord = recordDao.getRecordById(id);
+        return returnRecord;
+    }
+
+    @PostMapping(path = "/record")
+    public String createNewRecord(RecordDTO record) {
+        String newId = "";
+        Record newRecord = new Record();
+
+        newRecord = record.getRecord();
+
+
+
+
+
+        return newRecord.getId();
     }
 
 }
