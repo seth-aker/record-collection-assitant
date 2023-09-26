e63d540e1b200ba777148a02f6e5ca796bf67ce0
John Joyce <jjoycesommerfeld@gmail.com>
2023-09-26 13:14:01 -0700

    dao and test data

--- java/src/main/java/com/techelevator/dao/JdbcArtistDao.java
+++ java/src/main/java/com/techelevator/dao/JdbcArtistDao.java
@@ -2,6 +2,7 @@ package com.techelevator.dao;
 
 import com.techelevator.model.Artist;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,7 +15,22 @@ public class JdbcArtistDao {
     }
 
     public Artist getArtistById (String id) {
-        String sql = "SELECT "
+        String sql = "SELECT artist_id, artist_name " +
+                     "FROM artists WHERE artist_id = ?";
+
+        try {
+            SqlRowSet result = jdbcTemplate.queryForRowSet(sql, id);
+            if(result.next()) {
+                return
+            }
+        }
+    }
+
+    public Artist mapRowToArtist(SqlRowSet rowSet) {
+        Artist artist = new Artist();
+        artist.setArtistId(rowSet.getString("artist_id"));
+        artist.setName(rowSet.getString("artist_name"));
+
     }
 
 }
