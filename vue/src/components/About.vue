c112640e07ce98873c429b3a9c9b09b5bfa1ec22
Kirby Moore <kirbymoorecloud@gmail.com>
2023-09-27 17:59:33 -0400

    third push

--- vue/src/components/About.vue
+++ vue/src/components/About.vue
@@ -3,17 +3,56 @@
     <div class="container">
       <h2>TRENDING COLLECTIONS</h2>
       <div class="album-art">
-        <!-- Placeholder images for album art -->
-        <div class="album-placeholder"></div>
-        <div class="album-placeholder"></div>
-        <div class="album-placeholder"></div>
-         <div class="album-placeholder"></div>
-        <div class="album-placeholder"></div>
-         <div class="album-placeholder"></div>
-        <div class="album-placeholder"></div>
-         <div class="album-placeholder"></div>
-        <div class="album-placeholder"></div>
-        
+        <!-- Placeholder images for album art with text boxes -->
+        <div class="album-placeholder">
+          <img src="theWho.jpg" alt="Album 1" />
+          <div class="album-title">
+            <div class="album-title-text">Album title 1</div>
+          </div>
+        </div>
+        <div class="album-placeholder">
+          <img src="neilYoung.jpg" alt="Album 2" />
+          <div class="album-title">
+            <div class="album-title-text">Album Title 2</div>
+          </div>
+        </div>
+
+        <div class="album-placeholder">
+          <img src="funk.jpg" alt="Album 3" />
+          <div class="album-title">
+            <div class="album-title-text">Album Title 3</div>
+          </div>
+        </div>
+        <div class="album-placeholder">
+          <img src="sgtPepper.jpg" alt="Album 4" />
+          <div class="album-title">
+            <div class="album-title-text">Album Title 4</div>
+          </div>
+        </div>
+        <div class="album-placeholder">
+          <img src="rem.jpg" alt="Album 5" />
+          <div class="album-title">
+            <div class="album-title-text">Album Title 5</div>
+          </div>
+        </div>
+        <div class="album-placeholder">
+          <img src="theWho.jpg" alt="Album 6" />
+          <div class="album-title">
+            <div class="album-title-text">Album Title 6</div>
+          </div>
+        </div>
+        <div class="album-placeholder">
+          <img src="mj.jpg" alt="Album 7" />
+          <div class="album-title">
+            <div class="album-title-text">Album Title 7</div>
+          </div>
+        </div>
+           <div class="album-placeholder">
+          <img src="foals.jpg" alt="Album 8" />
+          <div class="album-title">
+            <div class="album-title-text">Album Title 8</div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -27,32 +66,51 @@ export default {
 
 <style scoped>
 .container {
-  margin-top: 200px; 
-  padding: 20px; 
-  background-color: rgba(255, 255, 255, 0.8);
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  text-align: center; 
+  margin-top: 200px;
+  padding: 20px;
+
+  text-align: center;
 }
 
 h2 {
-  font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-family: 'KEEPT___', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  font-size: 50px;
+  color: #40c5a4;
 }
 
 .album-art {
   text-align: center;
-  margin-top: 10px;
+  margin-top: 20px;
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
 }
 
 .album-placeholder {
-  width: 150px; 
-  height: 150px;
-  margin: 10px;
-  background-color: #ddd; 
-  
+  width: 280px; /* Increase the width to make the images bigger */
+  margin: 30px; /* Increase the margin to keep them spread apart */
+  padding: 10px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
+
+.album-placeholder img {
+  width: 100%; 
+}
+
+.album-title {
+  margin-top: 10px;
+  font-weight: bold;
+  color: #40c5a4;
+}
+
+.album-title-text {
+  font-size: 14px;
+  color: #40c5a4;
+}
+
 
 .right-section {
   padding: 100px;
@@ -62,5 +120,10 @@ h2 {
   flex-direction: column;
   align-items: center;
   justify-content: center;
+}
+
+@font-face {
+  font-family: 'KEEPT___';
+  src: url('@/assets/fonts/KEEPT___.TTF') format('truetype');
 }
 </style>
--- vue/src/components/Header.vue
+++ vue/src/components/Header.vue
@@ -1,11 +1,8 @@
 <template>
   <header class="right-section">
-    <div class="login-container">
-      <input type="text" class="login-input" id="login-input" placeholder="Login">
-      <input type="password" class="login-input" id="password-input" placeholder="Password">
-      <!-- Register button -->
-      <button class="register-button" id="register-button">Register</button>
-    </div>
+  <div></div>
+    
+    
     <h1>SLEEVES</h1>
   </header>
 </template>
@@ -18,25 +15,8 @@ export default {
 
 <style scoped>
 
-.login-container {
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  gap: 10px;
-}
 
-.login-input {
-  width: 100px; 
-  padding: 5px;
-}
 
-.register-button {
-  background-color: #007bff; 
-  color: #fff; 
-  padding: 5px 10px;
-  border: none;
-  cursor: pointer;
-}
 
 header {
     color: #fff;
@@ -46,12 +26,13 @@ header {
     left: 0;
     right: 0;
     top: 0;
+     
 }
 
 h1 {
     font-size: 130px;
     font-family: "KEEPT___", Arial, sans-serif;
-    color: #00d4ff;
+    color: #eff13f;
 }
 
 @font-face {
