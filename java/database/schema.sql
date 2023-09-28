2a3181e881a94be4c6425670ab2b12924e756eca
John Joyce <jjoycesommerfeld@gmail.com>
2023-09-27 17:09:53 -0700

    db update

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -57,67 +57,67 @@ CREATE TABLE user_record (
 	CONSTRAINT FK_record_id FOREIGN KEY (record_id) REFERENCES records(record_id)
 );
 
-CREATE TABLE genres (
-	genre_id SERIAL,
-	genre_name varChar(50) NOT NULL,
-	CONSTRAINT PK_genre_id PRIMARY KEY (genre_id)
-);
-
-CREATE TABLE artists (
-	artist_id int NOT NULL, --Not serial because it will be created by the API
-	artist_name varChar(50) NOT NULL,
-	CONSTRAINT PK_artist_id PRIMARY KEY (artist_id)
-);
-
-CREATE TABLE artist_genre (
-	genre_id int NOT NULL,
-	artist_id int NOT NULL,
-	CONSTRAINT PK_artist_id_genre_id PRIMARY KEY (genre_id, artist_id),
-	CONSTRAINT FK_artist_id FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
-	CONSTRAINT FK_genre_id FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
-);
-CREATE SEQUENCE seq_label_id
-	INCREMENT BY 1
-	START WITH 1001
-	NO MAXVALUE;
-
-CREATE TABLE labels (
-	label_id int NOT NULL DEFAULT nextval('seq_label_id'), 
-	label_name varChar(50) NOT NULL,
-	CONSTRAINT PK_label_id PRIMARY KEY (label_id)
-);
-
-CREATE TABLE record_label (
-	record_id varChar(30) NOT NULL,
-	label_id int NOT NULL,
-	CONSTRAINT PK_record_id_label_id PRIMARY KEY (record_id, label_id),
-	CONSTRAINT FK_record_id FOREIGN KEY (record_id) REFERENCES records(record_id),
-	CONSTRAINT FK_label_id FOREIGN KEY (label_id) REFERENCES labels(label_id)
-);
-
-CREATE TABLE record_artist (
-	record_id varChar(30) NOT NULL,
-	artist_id int NOT NULL,
-	CONSTRAINT PK_record_id_artist_id PRIMARY KEY (record_id, artist_id),
-	CONSTRAINT FK_record_id FOREIGN KEY (record_id) REFERENCES records(record_id),
-	CONSTRAINT FK_artist_id FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
-);
-
-CREATE TABLE tracks (
-	track_id varChar(30) NOT NULL,
-	title varChar(30) NOT NULL,
-	duration int,
-	track_number int,
-	CONSTRAINT PK_track_id PRIMARY KEY (track_id)
-);
-
-CREATE TABLE record_track (
-	record_id varChar(30) NOT NULL,
-	track_id varChar(30) NOT NULL,
-	CONSTRAINT PK_record_id_track_id PRIMARY KEY (record_id, track_id),
-	CONSTRAINT FK_record_id FOREIGN KEY (record_id) REFERENCES records(record_id),
-	CONSTRAINT FK_track_id FOREIGN KEY (track_id) REFERENCES tracks(track_id)
-);
+--CREATE TABLE genres (
+--	genre_id SERIAL,
+--	genre_name varChar(50) NOT NULL,
+--	CONSTRAINT PK_genre_id PRIMARY KEY (genre_id)
+--);
+--
+--CREATE TABLE artists (
+--	artist_id int NOT NULL, --Not serial because it will be created by the API
+--	artist_name varChar(50) NOT NULL,
+--	CONSTRAINT PK_artist_id PRIMARY KEY (artist_id)
+--);
+--
+--CREATE TABLE artist_genre (
+--	genre_id int NOT NULL,
+--	artist_id int NOT NULL,
+--	CONSTRAINT PK_artist_id_genre_id PRIMARY KEY (genre_id, artist_id),
+--	CONSTRAINT FK_artist_id FOREIGN KEY (artist_id) REFERENCES artists(artist_id),
+--	CONSTRAINT FK_genre_id FOREIGN KEY (genre_id) REFERENCES genres(genre_id)
+--);
+--CREATE SEQUENCE seq_label_id
+--	INCREMENT BY 1
+--	START WITH 1001
+--	NO MAXVALUE;
+--
+--CREATE TABLE labels (
+--	label_id int NOT NULL DEFAULT nextval('seq_label_id'),
+--	label_name varChar(50) NOT NULL,
+--	CONSTRAINT PK_label_id PRIMARY KEY (label_id)
+--);
+--
+--CREATE TABLE record_label (
+--	record_id varChar(30) NOT NULL,
+--	label_id int NOT NULL,
+--	CONSTRAINT PK_record_id_label_id PRIMARY KEY (record_id, label_id),
+--	CONSTRAINT FK_record_id FOREIGN KEY (record_id) REFERENCES records(record_id),
+--	CONSTRAINT FK_label_id FOREIGN KEY (label_id) REFERENCES labels(label_id)
+--);
+--
+--CREATE TABLE record_artist (
+--	record_id varChar(30) NOT NULL,
+--	artist_id int NOT NULL,
+--	CONSTRAINT PK_record_id_artist_id PRIMARY KEY (record_id, artist_id),
+--	CONSTRAINT FK_record_id FOREIGN KEY (record_id) REFERENCES records(record_id),
+--	CONSTRAINT FK_artist_id FOREIGN KEY (artist_id) REFERENCES artists(artist_id)
+--);
+--
+--CREATE TABLE tracks (
+--	track_id varChar(30) NOT NULL,
+--	title varChar(30) NOT NULL,
+--	duration int,
+--	track_number int,
+--	CONSTRAINT PK_track_id PRIMARY KEY (track_id)
+--);
+--
+--CREATE TABLE record_track (
+--	record_id varChar(30) NOT NULL,
+--	track_id varChar(30) NOT NULL,
+--	CONSTRAINT PK_record_id_track_id PRIMARY KEY (record_id, track_id),
+--	CONSTRAINT FK_record_id FOREIGN KEY (record_id) REFERENCES records(record_id),
+--	CONSTRAINT FK_track_id FOREIGN KEY (track_id) REFERENCES tracks(track_id)
+--);
 
 
 
