58b1986de67b667c39b63f3f8a959e170e43db64
John Joyce <jjoycesommerfeld@gmail.com>
2023-09-29 09:18:51 -0700

    new releases and store updates

---
+++ vue/src/views/NewReleases.vue
@@ -0,0 +1,21 @@
+<template>
+  
+    <record-list/>
+
+</template>
+
+<script>
+import RecordList from "./RecordList";
+
+export default {
+    name: "new-releases",
+    components: {
+        RecordList
+    }
+
+}
+</script>
+
+<style>
+
+</style>
