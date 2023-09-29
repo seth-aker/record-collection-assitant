3bd482c795880491f8d0b4e018e6af6fde6c6633
John Joyce <jjoycesommerfeld@gmail.com>
2023-09-29 09:12:14 -0700

    store update

--- vue/src/store/index.js
+++ vue/src/store/index.js
@@ -19,7 +19,17 @@ if(currentToken != null) {
 export default new Vuex.Store({
   state: {
     token: currentToken || '',
-    user: currentUser || {}
+    user: currentUser || {},
+    allReleases: [],
+    publicCollections: [],
+    userLibrary: [],
+    userCollections: [],
+    totalReleases: 0,
+    curPage: 1,
+    curPageReleases: [],
+    totalCollections: 0,
+    curPageCollections: [],
+    search:'',
   },
   mutations: {
     SET_AUTH_TOKEN(state, token) {
@@ -37,6 +47,37 @@ export default new Vuex.Store({
       state.token = '';
       state.user = {};
       axios.defaults.headers.common = {};
+    },
+    SET_ALL_RELEASES(state, data) {
+      state.allReleases = data;
+    },
+    SET_PUBLIC_COLLECTIONS(state, data) {
+      state.publicCollections = data;
+    },
+    SET_USER_LIBRARY(state, data) {
+      state.userLibrary = data;
+    },
+    SET_USER_COLLECTIONS(state, data) {
+      state.userCollections = data;
+    },
+    SET_TOTAL_RELEASES(state, count) {
+      state.totalReleases = count;
+    },
+    SET_CUR_PAGE(state, pageNum) {
+      state.curPage = pageNum;
+    },
+    SET_CUR_PAGE_RELEASES(state, releases) {
+      state.curPageReleases = releases;
+    },
+    SET_TOTAL_COLLECTIONS(state, count) {
+      state.totalCollections = count;
+    },
+    SET_CUR_PAGE_COLLECTIONS(state, collections) {
+      state.curPageCollections = collections;
+    },
+    UPDATE_SEARCH(state, term) {
+      state.search = term;
+      state.curPage = 1;
     }
   }
 })
