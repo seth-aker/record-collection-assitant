5a84cab53a3f6008a857d75918f762dde8e247e5
John Joyce <jjoycesommerfeld@gmail.com>
2023-09-29 11:27:52 -0700

    merge

--- vue/src/components/RecordList.vue
+++ vue/src/components/RecordList.vue
@@ -1,30 +1,30 @@
 <template>
     <div>
         <image-spinner v-show="showSpinner"/>
-        <div v-show="curPageProducts.length != 0 && !showSpinner">
+        <div v-show="curPageRecords.length != 0 && !showSpinner">
             <div id="record-row" 
                 v-for="curRecord in curPageRecords"
                 :key="curRecord.id"
                 :record="curRecord">
                 <div id="record-container">
-                    <album-art id="record-list-panel" :albumImageUrl="curRecord." :albumName="">
+                    <album-art id="record-list-panel" :albumImageUrl="curRecord.thumb" 
+                    :albumName="curRecord.title">
                 </div>
             </div>
         </div>
-        
-            <record-view/>
-        
+        <div class="no-items-msg" v-show="curPageRecords.length === 0 && !showSpinner">
+            No records found.
+        </div>
     </div>  
 </template>
 
 <script>
 import AlbumArt from './AlbumArt.vue';
-import RecordView from "./RecordView";
 
 export default {
     name: "record-list",
     components: {
-        RecordView,
+
         AlbumArt
     },
     data() {
@@ -44,7 +44,7 @@ export default {
         }
     },
     methods: {
-
+        
     },
     created() {
         this.$store.commit("SET_CUR_PAGE",1);
